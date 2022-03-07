a14dc4646ab8181209a6689a83ddecc5dcb78982
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-03-07 17:19:28 +0100

    fix text input widget

--- src/smart_register/core/fields/widgets/text.py
+++ src/smart_register/core/fields/widgets/text.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class SmartTextWidget(forms.TextInput):
-    def __init__(self, attrs=None, format=None):
+    def __init__(self, attrs=None):
         attrs = {"class": "appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500", "size": "10", **(attrs or {})}
-        super().__init__(attrs=attrs, format=format)
+        super().__init__(attrs=attrs)
