00b3e27f3d2d87841e85ba9c7881dccdd7baa90a
sax <s.apostolico@gmail.com>
2022-03-09 14:58:37 +0100

    add encryption

--- src/smart_register/registration/admin.py
+++ src/smart_register/registration/admin.py
@@ -1,5 +1,8 @@
-from admin_extra_buttons.decorators import button
+from admin_extra_buttons.decorators import view
 from cryptography.hazmat.primitives import serialization
+from django import forms
+from django.contrib import messages
+from django.db import models
 from django.shortcuts import render
 from import_export import resources
 
@@ -17,26 +20,42 @@ class RegistrationResource(resources.ModelResource):
 
 @register(Registration)
 class RegistrationAdmin(ImportExportMixin, SmartModelAdmin):
-    @button()
+    formfield_overrides = {models.TextField: {"widget": forms.PasswordInput}}
+    exclude = ("public_key",)
+    change_form_template = None
+
+    @view()
+    def removekey(self, request, pk):
+        self.object = self.get_object(request, pk)
+        self.object.public_key = ""
+        self.object.save()
+        self.message_user(request, "Encryption key removed", messages.WARNING)
+
+    @view()
     def generate_keys(self, request, pk):
-        ctx = self.get_common_context(request, pk)
+        ctx = self.get_common_context(
+            request, pk, title="Generate Private/Public Key pair to encrypt this Registration data"
+        )
         from cryptography.hazmat.backends import default_backend
         from cryptography.hazmat.primitives.asymmetric import rsa
 
-        private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
-        public_key = private_key.public_key()
+        if request.method == "POST":
+            ctx["title"] = "Key Pair Generated"
+            private_key = rsa.generate_private_key(public_exponent=65537, key_size=4096, backend=default_backend())
+            public_key = private_key.public_key()
 
-        private_pem = private_key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.PKCS8,
-            encryption_algorithm=serialization.NoEncryption(),
-        )
-        public_pem = public_key.public_bytes(
-            encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo
-        )
-
-        ctx["private_key"] = private_pem
-        ctx["public_key"] = public_pem
+            private_pem = private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption(),
+            )
+            public_pem = public_key.public_bytes(
+                encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo
+            )
+            self.object.public_key = public_pem
+            self.object.save()
+            ctx["private_key"] = private_pem
+            ctx["public_key"] = public_pem
 
         return render(request, "admin/registration/registration/keys.html", ctx)
 
--- src/smart_register/registration/models.py
+++ src/smart_register/registration/models.py
@@ -1,8 +1,14 @@
+import json
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 from django.conf import settings
 from django.contrib.postgres.fields import CICharField
 from django.db import models
 
 from smart_register.core.models import FlexForm
+from smart_register.core.utils import jsonfy, JSONEncoder
 
 
 class Registration(models.Model):
@@ -13,13 +19,29 @@ class Registration(models.Model):
     active = models.BooleanField(default=False)
     locale = models.CharField(max_length=10, choices=settings.LANGUAGES, default=settings.LANGUAGE_CODE)
 
-    public_key = models.TextField(blank=True, null=True)
+    public_key = models.TextField(
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         get_latest_by = "start"
 
     def __str__(self):
         return self.name
+
+    def add_record(self, data):
+        if self.public_key:
+            public_key = serialization.load_pem_public_key(self.public_key, backend=default_backend())
+            message = json.dumps(data, cls=JSONEncoder)
+            encrypted = public_key.encrypt(
+                message,
+                padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None),
+            )
+            payload = {"data": encrypted}
+        else:
+            payload = {"data": data}
+        return Record.objects.create(registration=self, data=jsonfy(payload))
 
 
 class Record(models.Model):
