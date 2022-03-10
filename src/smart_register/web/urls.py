97141359a221902c4b039a5a1ac63615c1903502
sax <s.apostolico@gmail.com>
2022-03-10 14:32:26 +0100

    add tests

--- src/smart_register/web/urls.py
+++ src/smart_register/web/urls.py
@@ -5,7 +5,7 @@ from .views import DataSetListView, HomeView, RegisterCompleView, RegisterView
 urlpatterns = [
     path("", HomeView.as_view(), name="index"),
     path("register/<int:pk>/", RegisterView.as_view(), name="register"),
-    path("register/", RegisterView.as_view(), name="register-last"),
+    path("register/", RegisterView.as_view(), name="register-latest"),
     path("register/complete/", RegisterCompleView.as_view(), name="register-done"),
     path("data/", DataSetListView.as_view(), name="result"),
 ]
