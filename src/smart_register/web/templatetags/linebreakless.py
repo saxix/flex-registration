1feb886615ab022a42778937b87a7b0a85ef65ed
Jan Romaniak <jan.romaniak@tivix.com>
2022-03-10 15:37:02 +0100

    vue init

---
+++ src/smart_register/web/templatetags/linebreakless.py
@@ -0,0 +1,9 @@
+import six
+from django import template
+from django.template.base import Node
+from django.utils.functional import keep_lazy
+
+
+register = template.Library()
+
+
--- src/smart_register/web/templatetags/smart_register.py
+++ src/smart_register/web/templatetags/smart_register.py
@@ -1,4 +1,6 @@
 from django.template import Library, Node
+import six
+from django.utils.functional import keep_lazy
 
 register = Library()
 
@@ -14,8 +16,25 @@ class EscapeScriptNode(Node):
         return escaped_out
 
 
+class LinebreaklessNode(Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        out = self.nodelist.render(context)
+        escaped_out = out.replace("\n", "")
+        return escaped_out
+
+
 @register.tag()
 def escapescript(parser, token):
     nodelist = parser.parse(("endescapescript",))
     parser.delete_first_token()
     return EscapeScriptNode(nodelist)
+
+
+@register.tag()
+def linebreakless(parser, token):
+    nodelist = parser.parse(("endlinebreakless",))
+    parser.delete_first_token()
+    return LinebreaklessNode(nodelist)
