7dcccc8cbf432d53b6ad0d7d289238289fdd6323
sax <s.apostolico@gmail.com>
2022-03-10 07:13:58 +0100

    fixes tests

--- tests/fields/test_select_field.py
+++ tests/fields/test_select_field.py
@@ -5,7 +5,7 @@ from smart_register.core.models import OptionSet
 
 
 @pytest.fixture
-def options(db):
+def complex(db):
     return OptionSet.objects.get_or_create(
         name="italian_locations",
         defaults={
@@ -15,11 +15,26 @@ def options(db):
     )[0]
 
 
-def test_select(options):
+@pytest.fixture
+def flat(db):
+    return OptionSet.objects.get_or_create(
+        name="italian_locations",
+        defaults={
+            "data": "Rome\nMilan",
+        },
+    )[0]
+
+
+def test_select_complex(complex):
     fld = SelectField(choices=[["italian_locations", "italian_locations"]])
     assert fld.choices == [["1", "Rome"], ["2", "Milan"]]
 
 
-def test_error():
+def test_select_flat(flat):
+    fld = SelectField(choices=[["italian_locations", "italian_locations"]])
+    assert fld.choices == [["rome", "Rome"], ["milan", "Milan"]]
+
+
+def test_error(db):
     with pytest.raises(ValueError):
         SelectField(choices="italian_locations")
