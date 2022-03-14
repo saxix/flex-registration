2d5299e171a5c5aeac18a12233268658352af9f4
Jan Romaniak <jan.romaniak@tivix.com>
2022-03-14 18:25:28 +0100

    call upgrade

--- docker/entrypoint-local.sh
+++ docker/entrypoint-local.sh
@@ -2,6 +2,8 @@
 set -e
 
 if [ $# -eq 0 ]; then
+
+    python manage.py upgrade
     exec gunicorn smart_register.config.wsgi -c /code/gunicorn_config.py
 else
     case "$1" in
@@ -10,6 +12,7 @@ else
           do echo "waiting for database"; sleep 2; done;
         python manage.py collectstatic --no-input
         python manage.py migrate
+
         python manage.py runserver 0.0.0.0:8000
 
         ;;
