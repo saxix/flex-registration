c6cd6cb04427388aeeb8e12cb8206c3d80538269
sax <s.apostolico@gmail.com>
2022-03-21 18:19:02 +0100

    UX updates

--- src/smart_register/web/context_processors.py
+++ src/smart_register/web/context_processors.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 def smart(request):
     return {
-        "smart": {
+        "project": {
             "build_date": os.environ.get("BUILD_DATE", ""),
             "version": os.environ.get("VERSION", ""),
             "debug": settings.DEBUG,
