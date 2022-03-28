9133669dd278b80654bdda2ea9ce2e7e4d62d333
sax <s.apostolico@gmail.com>
2022-03-28 13:27:19 +0200

    updates tests

--- tests/browser/test_f_register.py
+++ tests/browser/test_f_register.py
@@ -41,15 +41,7 @@ def test_ukr(live_server, selenium, registration):
 
     wait_for(selenium, By.CSS_SELECTOR, "label[for=id_household-0-residence_status_h_c_0]").click()
     wait_for(selenium, By.CSS_SELECTOR, "input[type=submit]").click()
-    #
-    # selenium.find_element(By.ID, "select2-id_household-0-admin1_h_c-container").click()
-    # time.sleep(1)
-    # selenium.find_element(By.ID, "select2-id_household-0-admin2_h_c-container").click()
-    # time.sleep(1)
-    # selenium.find_element(By.ID, "select2-id_household-0-admin3_h_c-container").click()
-    # find_by_css('fieldset.admin1_h_c .select2 .selection .select2-selection').click()
 
-    # find_by_css("fieldset.admin1_h_c .select2")
     LOCATIONS = ["Автономна Республіка Крим", "Бахчисарайський", "Севастопільська"]
 
     find_by_css("fieldset.admin1_h_c .select2 .selection .select2-selection").click()
@@ -78,19 +70,6 @@ def test_ukr(live_server, selenium, registration):
 
     wait_for(selenium, By.CSS_SELECTOR, "input[type=submit]").click()
 
-    #
-    # find_by_css("fieldset.admin2_h_c .select2").click()
-    # find_by_css('.select2-search__field').send_keys('Бахчисарайський')
-    # time.sleep(1)
-    # find_by_css('.select2-search__field').send_keys(Keys.ENTER)
-    # time.sleep(2)
-    #
-    # find_by_css("fieldset.admin3_h_c .select2").click()
-    # find_by_css('.select2-search__field').send_keys('Севастопільська')
-    # time.sleep(1)
-    # find_by_css('.select2-search__field').send_keys(Keys.ENTER)
-    # time.sleep(1)
-
     find_by_css("input[name=household-0-size_h_c").send_keys("1")
     find_by_css('input[name="individuals-0-given_name_i_c"]').send_keys("Given Name")
     find_by_css('input[name="individuals-0-family_name_i_c"]').send_keys("Family Name")
@@ -106,82 +85,3 @@ def test_ukr(live_server, selenium, registration):
     wait_for(selenium, By.CSS_SELECTOR, "input[type=submit]").click()
 
     find_by_css(f"a[href='{registration.get_absolute_url()}']").click()
-
-
-#
-# @pytest.mark.selenium
-# def test_login_pwd_success(live_server, selenium, user):
-#     from testutils.factories import CustomerFactory, SubscriptionFactory
-#     customer = CustomerFactory(user=user)
-#     SubscriptionFactory(customer=customer)
-#
-#     selenium.implicitly_wait(3)
-#     selenium.get(f'{live_server.url}/')
-#     dim = selenium.get_window_size()
-#     selenium.set_window_size(1100, dim['height'])
-#
-#     wait_for(selenium, By.ID, 'id_login_email')
-#     selenium.find_element(By.ID, 'toggler1').click()
-#
-#     selenium.find_element(By.NAME, 'auth-username').send_keys(user.username)
-#     selenium.find_element(By.NAME, 'auth-password').send_keys(user._password)
-#     selenium.find_element(By.ID, 'login-button-pwd').click()
-#
-#     wait_for(selenium, By.PARTIAL_LINK_TEXT, 'ADDRESSBOOK')
-#     assert '/summary/' in selenium.current_url
-#
-#
-# @pytest.mark.selenium
-# def test_login_mail_link(live_server, selenium, user, mailoutbox):
-#     from testutils.factories import CustomerFactory, SubscriptionFactory
-#     customer = CustomerFactory(user=user)
-#     SubscriptionFactory(customer=customer)
-#
-#     selenium.implicitly_wait(3)
-#     selenium.get(f'{live_server.url}/')
-#     dim = selenium.get_window_size()
-#     selenium.set_window_size(1100, dim['height'])
-#
-#     wait_for(selenium, By.ID, 'id_login_email')
-#     # selenium.find_element(By.ID, 'toggler1').click()
-#
-#     selenium.find_element(By.NAME, 'email').send_keys(user.email)
-#     selenium.find_element(By.NAME, 'send-email').click()
-#
-#     wait_for_url(selenium, 'email/sent/')
-#
-#     assert len(mailoutbox) == 1
-#
-#
-# @pytest.mark.selenium
-# def test_login_2fa(live_server, selenium, user, monkeypatch):
-#     from testutils.factories import CustomerFactory, SubscriptionFactory
-#     customer = CustomerFactory(user=user)
-#     SubscriptionFactory(customer=customer)
-#     from django_otp.plugins.otp_totp.models import TOTPDevice
-#     from two_factor.utils import totp_digits
-#     device = TOTPDevice.objects.create(user=user, key='123',
-#                                        tolerance=100, t0=0,
-#                                        step=30, drift=0,
-#                                        digits=totp_digits(),
-#                                        name='default')
-#
-#     # monkeypatch.setattr('django_otp.match_token', lambda x, y: device)
-#     monkeypatch.setattr('django_otp.forms.match_token', lambda x, y: device)
-#
-#     selenium.implicitly_wait(3)
-#     selenium.get(f'{live_server.url}/')
-#     dim = selenium.get_window_size()
-#     selenium.set_window_size(1100, dim['height'])
-#
-#     wait_for(selenium, By.ID, 'id_login_email')
-#     selenium.find_element(By.ID, 'toggler1').click()
-#
-#     selenium.find_element(By.NAME, 'auth-username').send_keys(user.username)
-#     selenium.find_element(By.NAME, 'auth-password').send_keys(user._password)
-#     selenium.find_element(By.ID, 'login-button-pwd').click()
-#     selenium.find_element(By.NAME, 'token-otp_token').send_keys('123456')
-#     selenium.find_element(By.ID, 'send-token-button').click()
-#
-#     wait_for_url(selenium, '/summary/')
-#     assert '/summary/' in selenium.current_url
