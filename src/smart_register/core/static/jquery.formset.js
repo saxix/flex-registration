85a6e1f68b1bbdf776fc12077f3b84d0b17fd802
sax <s.apostolico@gmail.com>
2022-04-03 08:39:42 +0200

    updates

--- src/smart_register/core/static/jquery.formset.js
+++ src/smart_register/core/static/jquery.formset.js
@@ -182,7 +182,7 @@
             // FIXME: Perhaps using $.data would be a better idea?
             options.formTemplate = template;
 
-            var addButtonHTML = '<a class="' + options.addCssClass + '" href="javascript:void(0)">' + options.addText + '</a>';
+            var addButtonHTML = '<a rel="nofollow" class="' + options.addCssClass + '" href="javascript:void(0)">' + options.addText + '</a>';
             if (options.addContainerClass) {
                 // If we have a specific container for the "add" button,
                 // place it as the last child of that container:
