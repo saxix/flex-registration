03276fd9041c77113f8ad666246ca8fdd2315e85
sax <s.apostolico@gmail.com>
2022-04-10 14:27:39 +0200

    updates deployment files

--- src/smart_register/config/settings.py
+++ src/smart_register/config/settings.py
@@ -205,7 +205,8 @@ USE_TZ = True
 STATIC_URL = env("STATIC_URL")
 STATIC_ROOT = env("STATIC_ROOT")
 # STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATICFILES_STORAGE = env("STATICFILES_STORAGE")
 
 STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, "web/static"),
