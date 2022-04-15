c3a8f45c69cd5a0739da851651ba7b28aa2c38e2
sax <s.apostolico@gmail.com>
2022-04-15 18:05:32 +0100

    wizard mode

--- src/smart_register/web/static/wizard.js
+++ src/smart_register/web/static/wizard.js
@@ -1,12 +1,17 @@
 (function ($) {
     $(function () {
+        var buttons = "<input type=\"button\" id=\"wizard-prev\" value=\"<<\"/>" +
+            "<div class='px-3 inline-flex' id=\"currentPage\"></div>" +
+            "<input type=\"button\" id=\"wizard-next\" value=\">>\"/>";
+        $(".submit-row").prepend(buttons);
         var currentPage = 0;
+        var maxPages = $('.formset-config').length;
         $("[data-page]").hide();
         $("[data-page=\"0\"]").show();
         var update = function () {
             $("#wizard-prev").prop("disabled", currentPage === 0);
             $("#wizard-next").prop("disabled", currentPage === maxPages);
-            $("#currentPage").html((currentPage+1) + " / " + (maxPages +1));
+            $("#currentPage").html((currentPage + 1) + " / " + (maxPages + 1));
         };
         update();
         $("#wizard-next").on("click", function () {
@@ -19,11 +24,5 @@
             $("[data-page=\"" + --currentPage + "\"]").show();
             update();
         });
-
-        if (formsetConfig.length > 0) {
-            configureFormsets(formsetConfig);
-        }
-        $("input:checked[onchange]").trigger("change");
-        $("#spinner").hide();
     });
 })($);
