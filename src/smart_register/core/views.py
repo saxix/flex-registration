a99b6a56dee3dab4287d3a4546e4c73fb484b694
sax <s.apostolico@gmail.com>
2022-04-20 21:01:14 +0200

    fixes optionset etag

--- src/smart_register/core/views.py
+++ src/smart_register/core/views.py
@@ -1,5 +1,8 @@
+import time
+
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.utils.cache import get_conditional_response
 from django.utils.decorators import method_decorator
 from django.utils.translation import get_language
 from django.views.decorators.cache import cache_page
@@ -7,13 +10,10 @@ from django.views.generic.list import BaseListView
 
 from smart_register.core.models import OptionSet
 from smart_register.core.utils import get_etag
+from smart_register.state import state
 
 
-def filter_optionset(obj, request, parent=None):
-    term = request.GET.get("q")
-    parent = request.GET.get("parent", parent)
-    pk = request.GET.get("pk")
-
+def filter_optionset(obj, pk, term, lang, parent=None):
     def _filter(record):
         valid = True
         if pk:
@@ -24,8 +24,6 @@ def filter_optionset(obj, request, parent=None):
             valid = valid and record["parent"] == parent
         return valid
 
-    lang = get_language()
-    etag = get_etag(request, obj.pk, obj.version, lang, term, parent, pk)
     data = {
         "results": [
             {
@@ -37,17 +35,37 @@ def filter_optionset(obj, request, parent=None):
             if _filter(record)
         ],
     }
-    response = JsonResponse(data)
-    response["Cache-Control"] = "public, max-age=315360000"
-    response["ETag"] = etag
-    return response
+    return data
 
 
 @method_decorator(cache_page(60 * 60), name="dispatch")
 class OptionsListView(BaseListView):
     def get(self, request, *args, **kwargs):
         name = self.kwargs["name"]
-        # pk = int(self.kwargs["pk"])
         parent = self.kwargs.get("parent", None)
+        lang = get_language()
+        term = request.GET.get("q")
+        parent = request.GET.get("parent", parent)
+        pk = request.GET.get("pk")
+
         obj = get_object_or_404(OptionSet, name=name)
-        return filter_optionset(obj, request, parent)
+
+        if state.collect_messages:
+            etag = get_etag(request, time.time())
+        else:
+            etag = get_etag(
+                request,
+                obj.pk,
+                obj.version,
+                lang,
+                term,
+                parent,
+                pk,
+            )
+        response = get_conditional_response(request, str(etag))
+        if response is None:
+            data = filter_optionset(obj, pk, term, lang, parent)
+            response = JsonResponse(data)
+            response["Cache-Control"] = "public, max-age=315360000"
+            response["ETag"] = etag
+        return response
