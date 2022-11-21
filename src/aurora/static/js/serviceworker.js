ec00f69c4c0c82e92149aa966e0f46bf3167259d
Konrad Marzec <konrad.marzec1991@gmail.com>
2022-11-21 23:43:33 +0100

    Add indexeddb support

--- src/aurora/static/js/serviceworker.js
+++ src/aurora/static/js/serviceworker.js
@@ -1,60 +1,153 @@
-const cacheName = "v1";
+const version = 2;
+
+let staticName = `staticCache-${version}`;
+let dynamicName = `dynamicCache`;
+let imageName = `imageCache-${version}`;
+
+let assets = [
+    "/",
+    "/registrations/",
+    // "/i18n/en-us/",
+    "/serviceworker.js",
+    // "/static/admin/debug.css",
+    "/static/registration/auth.js",
+    "/static/page.min.js",
+    "/static/edit.min.js",
+    "/static/i18n/i18n_edit.js",
+    "/static/smart.js",
+    "/static/registration/survey.min.js",
+    "/static/admin/js/vendor/jquery/jquery.js",
+    "/static/base.css",
+    "/static/staff-toolbar.css",
+    // "/static/hope1.webp",
+    "https://code.jquery.com/jquery-3.6.0.min.js",
+    "/api/project/",
+];
+
+let imageAssets = [
+  "/static/images/cat.jpeg"
+];
 
 
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(resources);
+const handleFetchResponse = (fetchResponse, request) => {
+  let type = fetchResponse.headers.get('content-type');
+
+  if (type && type.match(/^image\//i)) {
+    console.log(`SAVE ${request.url} in ${imageName}`);
+
+    return caches.open(imageName).then((cache) => {
+      cache.put(request, fetchResponse.clone());
+      return fetchResponse;
+    });
+  } else {
+    console.log(`SAVE ${request.url} in ${dynamicName}`);
+
+    return caches.open(dynamicName).then((cache) => {
+      cache.put(request, fetchResponse.clone());
+      return fetchResponse;
+    });
+  }
 };
 
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
+  console.log(`Version ${version} installed - caching started`);
+
   event.waitUntil(
-    addResourcesToCache([
-      "/",
-      "/registrations/",
-      // "/i18n/en-us/",
-      "/serviceworker.js",
-      // "/static/admin/debug.css",
-      "/static/registration/auth.js",
-      "/static/page.min.js",
-      "/static/edit.min.js",
-      "/static/i18n/i18n_edit.js",
-      "/static/smart.js",
-      "/static/registration/survey.min.js",
-      "/static/admin/js/vendor/jquery/jquery.js",
-      "/static/base.css",
-      "/static/staff-toolbar.css",
-      // "/static/hope1.webp",
-      "https://code.jquery.com/jquery-3.6.0.min.js",
-      "/api/project/",
-    ])
+    caches.open(staticName).then(cache => {
+        cache.addAll(assets).then(
+            () => {
+              console.log(`${staticName} has been updated.`);
+            },
+            err => {
+              console.warn(`Failed to update ${staticName}, ${err}`)
+            }
+        )
+    }).then(() => {
+       caches.open(imageName).then(cache => {
+           cache.addAll(imageAssets).then(
+            () => {
+              console.log(`${imageName} has been updated.`);
+            },
+            err => {
+              console.warn(`failed to update ${staticName}.`);
+            }
+          );
+       });
+    })
   );
 });
 
 
-const cacheFirst = async (request) => {
-  const responseFromCache = await caches.match(request);
-  // const responseClone = responseFromCache.clone();
+self.addEventListener("activate", event => {
+   console.log("Activated - previous cache will be cleared");
 
-  console.log("responseFromCache", responseFromCache);
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-  return fetch(request, { mode: 'no-cors'}).then(response => {
-    caches.open(cacheName).then(cache => {
-      cache.put(request, response.clone());
-    })
-  });
-};
+   event.waitUntil(
+     caches.keys().then(keys => {
+        return Promise.all(
+            keys.filter(key => key !== staticName && key !== imageName).map(key => caches.delete(key))
+        );
+     })
+   );
+});
 
 
-self.addEventListener("fetch", (event) => {
-    if (navigator.onLine) {
-        console.log("Fetching from SERVER");
-        return fetch(event.request);
-    } else {
-        console.log(`Fetching from service worker url=${event.request.url}`);
-        return event.respondWith(cacheFirst(event.request));
+self.addEventListener("fetch", event => {
+    console.log(`Fetching request for: ${event.request.url}`);
+
+    if (!event.request.url.endsWith("/")) {
+        event.request.url = event.request.url + "/";
     }
 
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            caches.match(event.request).then(cacheResponse => {
+                return cacheResponse ||
+                    Promise.resolve().then(() => {
+                        let options = {
+                            mode: event.request.mode,
+                            cache: "no-cache"
+                        };
+
+                        if (!event.request.url.startsWith(location.origin)) {
+                            options.mode = "cors";
+                            options.credentials = "omit";
+                        }
+
+                        console.log("fetchResponse.status");
+
+                        return fetch(event.request.url, options).then(
+                            fetchResponse => {
+                                if (fetchResponse.ok) {
+                                    console.log(fetchResponse);
+                                    console.log("fetchResponse.status", fetchResponse.status);
+                                    return handleFetchResponse(fetchResponse, event.request);
+                                }
+
+                                if (fetchResponse.status === 404) {
+                                    if (
+                                        event.request.url.match(/\.jpg$/i) ||
+                                        event.request.url.match(/\.png$/i)
+                                    ) {
+                                        return caches.open(imageName).then((cache) => {
+                                            return cache.match('/img/distracted-boyfriend.jpg');
+                                        });
+                                    } else {
+                                        return caches.open(staticName).then(cache => {
+                                            return cache.match("/offline/")
+                                        })
+                                    }
+                                }
+                            },
+                            err => {
+                                return caches.open(staticName).then(cache => {
+                                    return cache.match("/offline/")
+                                })
+                            }
+                        );
+                    });
+
+            })
+        })
+    );
 });
--- src/aurora/web/views/site.py
+++ src/aurora/web/views/site.py
@@ -28,6 +28,10 @@ def error_404(request, exception):
     return TemplateResponse(request, "404.html", status=404, headers={"Session-Token": settings.DJANGO_ADMIN_URL})
 
 
+def offline(request):
+    return render(request, "offline.html")
+
+
 def get_active_registrations():
     registrations = Registration.objects.filter(active=True, show_in_homepage=True)
     return registrations
