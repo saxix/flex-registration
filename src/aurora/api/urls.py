9d439dcce14bd8cda13df10606870b87d2154489
sax <s.apostolico@gmail.com>
2023-02-06 07:10:00 +0100

    counter refresh API

--- src/aurora/api/urls.py
+++ src/aurora/api/urls.py
@@ -11,17 +11,20 @@ from .viewsets import (
     ValidatorViewSet,
     project_info,
 )
+from .viewsets.counter import CounterViewSet
 
 app_name = "api"
 
 router = DefaultRouter()
-router.register(r"user", UserViewSet)
-router.register(r"registration", RegistrationViewSet)
+router.register(r"counter", CounterViewSet)
+router.register(r"field", FlexFormFieldViewSet)
 router.register(r"form", FlexFormViewSet)
 router.register(r"formset", FormSetViewSet)
-router.register(r"field", FlexFormFieldViewSet)
-router.register(r"validator", ValidatorViewSet)
+router.register(r"registration", RegistrationViewSet)
 router.register(r"template", TemplateViewSet)
+router.register(r"template", TemplateViewSet)
+router.register(r"user", UserViewSet)
+router.register(r"validator", ValidatorViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
