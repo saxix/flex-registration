5c281172ded68e391ab3ad5b7b062ae8ce6c2945
sax <s.apostolico@gmail.com>
2023-03-18 21:22:59 +0100

    updates

--- src/aurora/registration/admin/__init__.py
+++ src/aurora/registration/admin/__init__.py
@@ -1,7 +1,8 @@
 from django.contrib.admin import register
 
-from .record import Record, RecordAdmin
-from .registration import Registration, RegistrationAdmin
+from .record import RecordAdmin
+from .registration import RegistrationAdmin
+from ..models import Record, Registration
 
 register(Registration)(RegistrationAdmin)
 register(Record)(RecordAdmin)
